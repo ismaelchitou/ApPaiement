503010036da10a1dfdfca29edf353ab2be41709f
CHITOU Akambi Mouhammad Ismael <300131785@monboreal.ca>
2024-12-08 22:51:25 -0500

    Creation de la classe fille PayPal

---
+++ AppPaiement/Paypal.cs
@@ -0,0 +1,25 @@
+﻿using AppPaiements;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppPaiement
+{
+    internal class Paypal :Paiement
+    {
+        public string Courriel { get; set; }
+        
+        public Paypal(double montant, string desc,string courriel):
+            base(montant,desc)
+        {
+            Courriel = courriel;
+        }
+
+        public override void AfficherDetails()
+        {
+            Console.WriteLine($"le infos du Paypal Montan:{Montant},Description{Description},Courriel{Courriel}");
+        }
+    }
+}
--- AppPaiement/Program.cs
+++ AppPaiement/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace AppPaiements
+﻿using AppPaiement;
+
+namespace AppPaiements
 {
     internal class Program
     {
@@ -8,6 +10,8 @@
             CarteCredit achat2 = new CarteCredit(7.50, "Magasin", 546382920);
             CarteCredit achat3 = new CarteCredit(39.45, "Telephone", 363287423);
 
+            Paypal transaction1 = new Paypal(150.00, "frais de scolarte", "akwame@gmail.com");
+            Paypal transaction2 = new Paypal(2000, "Achat voiture", "chitouismael@gmail.com");
         }
     }
 }
