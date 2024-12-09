5f2aff791d893c58bc8689cb903ee5f5759aa3a2
CHITOU Akambi Mouhammad Ismael <300131785@monboreal.ca>
2024-12-08 22:36:42 -0500

    Creation de la classe fille Cartecredit

---
+++ AppPaiement/Cartecredit.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppPaiements
+{
+    internal class CarteCredit : Paiement
+    {
+        public int NumeroCarte { get; set; }
+
+        public CarteCredit(double montant, string desc, int numeroCarte) :
+            base(montant, desc)
+        {
+            NumeroCarte = numeroCarte;
+        }
+        public override void AfficherDetails()
+        {
+            Console.WriteLine($"Les details du paiements Montan:{Montant},Description:{Description},Numero de carte:{NumeroCarte}");
+        }
+    }
+}
--- AppPaiement/Program.cs
+++ AppPaiement/Program.cs
@@ -1,7 +1,13 @@
-﻿internal class Program
+﻿namespace AppPaiements
 {
-    private static void Main(string[] args)
+    internal class Program
     {
-        Console.WriteLine("Hello, World!");
+        static void Main(string[] args)
+        {
+            CarteCredit achat1 = new CarteCredit(150.00, "dollarama", 567891234);
+            CarteCredit achat2 = new CarteCredit(7.50, "Magasin", 546382920);
+            CarteCredit achat3 = new CarteCredit(39.45, "Telephone", 363287423);
+
+        }
     }
 }
