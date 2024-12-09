49842b2f1fe49e42d31a023822a0eacfd70fd127
CHITOU Akambi Mouhammad Ismael <300131785@monboreal.ca>
2024-12-08 23:03:10 -0500

    Ajout de la Classe Utlisateur

---
+++ AppPaiement/Utlisateur.cs
@@ -0,0 +1,24 @@
+﻿using AppPaiements;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppPaiement
+{
+    internal class Utlisateur
+    {
+        public string Nom { get; set; }
+        public List<Paiement> paiements { get; set; }
+        public Utlisateur(string nom)
+        {
+            Nom = nom;
+        }
+        public void AfficherInfos()
+        {
+            Console.WriteLine($"Utilisateur: {Nom}");
+            Console.WriteLine("Liste des paiements:");
+        }
+    }
+}
