5e215894e807a41b6f0e2e068e4070066eb2bad4
CHITOU Akambi Mouhammad Ismael <300131785@monboreal.ca>
2024-12-08 22:18:56 -0500

    Creation de la classe mere abstraite Paiement

---
+++ AppPaiement/Paiement.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppPaiements
+{
+    internal class Paiement
+    {
+        public double Montant { get; set; }
+        public string Description { get; set; }
+
+        public Paiement(double montant, string desc)
+        {
+            Montant = montant;
+            Description = desc;
+        }
+        public virtual void AfficherDetails()
+        {
+            Console.WriteLine($"Le paiement est : Montant:{Montant},Description:{Description}");
+        }
+    }
+}
